61251cdc9a884d2e7f7fd80af115044dc57e0538
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-11 06:43:37 +0100

    init commit

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList, SafeArea } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { COLORS, NFTData } from '../constants';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
+  )
+}
+
+export default Home;
